395a5f487b424e5b5bb750e027829a12976854b5
Rajveer Singh Panwar <rajveersinghop0055@gamil.com>
2025-03-18 18:58:55 +0530

    pushing the gsapCode

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import GsapFrom from './pages/GsapFrom'
 import GsapTo from './pages/GsapTo'
 import GsapTimeline from './pages/GsapTimeLine'
+import GsapStagger from './pages/GsapStagger'
+import GsapScrollTrigger from './pages/GsapScrollTriger'
+import GsapText from './pages/GsapText'
 
 function App() {
   return (
     <div>
-  {/* //<GsapFrom/> */}
-      {/* <GsapTo/> */}
+  {/* <GsapFrom/>
+      <GsapTo/>
       <GsapTimeline/>
+       <GsapStagger/> 
+      <GsapScrollTrigger/>
+      */}
+      <GsapText/>
     </div>
   )
 }
